b10c093b50e69defa9c30b05d0e2321cc18f34bf
GowthamiAnne <annegowthami10@gmail.com>
2026-01-16 04:09:51 +0000

    Update Main.java
    Profile related changes

--- LoyaltyService/LoyaltyService/src/main/java/com/airline/loyalty/Main.java
+++ LoyaltyService/LoyaltyService/src/main/java/com/airline/loyalty/Main.java
@@ -1,13 +1,47 @@
 package com.airline.loyalty;
 
-import io.vertx.core.Launcher;
+import io.vertx.config.ConfigRetriever;
+import io.vertx.config.ConfigRetrieverOptions;
+import io.vertx.config.ConfigStoreOptions;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+
 
 public class Main {
+
     public static void main(String[] args) {
-        Launcher.main(new String[]{
-            "run", 
-            "com.airline.loyalty.MainVerticle", 
-            "-conf", "src/main/resources/config.json"
+        Vertx vertx = Vertx.vertx();
+
+        String profile = System.getProperty(
+            "vertx.profile",
+            System.getenv().getOrDefault("VERTX_PROFILE", "dev")
+        );
+        
+        
+
+        ConfigStoreOptions fileStore = new ConfigStoreOptions()
+            .setType("file")
+            .setConfig(new JsonObject()
+                .put("path", "src/main/resources/application-" + profile + ".json"));
+
+        System.out.println(profile+"profile");
+        ConfigRetriever retriever = ConfigRetriever.create(
+            vertx,
+            new ConfigRetrieverOptions().addStore(fileStore)
+        );
+
+        retriever.getConfig(ar -> {
+            if (ar.failed()) {
+                ar.cause().printStackTrace();
+                return;
+            }
+
+            DeploymentOptions options =
+                new DeploymentOptions().setConfig(ar.result());
+
+            vertx.deployVerticle(new MainVerticle(), options);
         });
     }
 }
+
